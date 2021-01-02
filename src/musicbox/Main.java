8bb3fbb1e00ed15232347a054e709e03fdba4933
geekon <kacperpiasta@gmail.com>
2021-01-02 20:39:03 +0100

    bind key up events to windupKey animations

--- src/musicbox/Main.java
+++ src/musicbox/Main.java
@@ -13,11 +13,28 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.math.BigDecimal;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static javafx.animation.Animation.INDEFINITE;
+import static javafx.animation.Animation.Status.RUNNING;
+import static javafx.animation.Interpolator.EASE_BOTH;
+import static javafx.animation.Interpolator.LINEAR;
+import static javafx.scene.input.KeyCode.UP;
+import static javafx.scene.input.KeyEvent.KEY_PRESSED;
+import static javafx.scene.input.KeyEvent.KEY_RELEASED;
 
 public class Main extends Application {
 
@@ -27,7 +44,15 @@ public class Main extends Application {
     private static final String AUDIO01_FILENAME = "";
     private static final String AUDIO02_FILENAME = "";
 
-    private ImageView musicBox;
+    private static final double MAX_RATE = 20000;
+    private static final double RATE_MODIFIER = 0.25;
+
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
+    private boolean running = false;
+
+    private RotateTransition rotator;
+
     private ImageView windupKey;
     private TextField display;
     private ToggleGroup toggleGroup;
@@ -35,15 +60,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        musicBox = createMusicBox();
         Scene scene = createScene();
         scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("favicon.png")));
         primaryStage.setTitle("MusicBox");
         primaryStage.show();
-        primaryStage.show();
-        windupKey.setOnMouseClicked(event -> setWindupKeyMouseClicked());
+        setupWindupKeyOnScroll();
+        scene.addEventFilter(KEY_PRESSED, this::handleWindupKeyOnKeyUpPressed);
+        scene.addEventFilter(KEY_RELEASED, this::handleWindupKeyOnKeyUpReleased);
         toggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
             if (toggleGroup.getSelectedToggle() == null) {
                 toggleGroup.selectToggle(oldValue);
@@ -58,6 +83,7 @@ public class Main extends Application {
 
     private Scene createScene() {
         AnchorPane root = new AnchorPane();
+        ImageView musicBox = createMusicBox();
         windupKey = createWindupKey();
         display = createDisplay();
         toggleGroup = new ToggleGroup();
@@ -71,7 +97,7 @@ public class Main extends Application {
     private SubScene createWindupScene() {
         StackPane root = new StackPane();
         root.getChildren().addAll(windupKey);
-        SubScene scene = new SubScene(root, 140, 120, true, SceneAntialiasing.BALANCED);
+        SubScene scene = new SubScene(root, 140, 120, false, SceneAntialiasing.BALANCED);
         scene.setLayoutX(560);
         scene.setLayoutY(540);
         scene.setCamera(new PerspectiveCamera());
@@ -99,9 +125,9 @@ public class Main extends Application {
         ToggleButton button = new ToggleButton();
         button.setLayoutX(79);
         button.setLayoutY(566);
-        button.setId("firstButton");
         button.setToggleGroup(toggleGroup);
         button.setSelected(true);
+        button.setFocusTraversable(false);
         return button;
     }
 
@@ -109,24 +135,52 @@ public class Main extends Application {
         ToggleButton button = new ToggleButton();
         button.setLayoutX(484);
         button.setLayoutY(566);
-        button.setId("secondButton");
         button.setToggleGroup(toggleGroup);
+        button.setFocusTraversable(false);
         return button;
     }
 
     private RotateTransition createRotator(Node windupKey) {
-        RotateTransition rotator = new RotateTransition(Duration.millis(10000), windupKey);
+        RotateTransition rotator = new RotateTransition(Duration.millis(10000000), windupKey);
         rotator.setAxis(Rotate.X_AXIS);
         rotator.setFromAngle(0);
         rotator.setToAngle(360);
-        rotator.setInterpolator(Interpolator.LINEAR);
-        rotator.setCycleCount(RotateTransition.INDEFINITE);
+        rotator.setInterpolator(LINEAR);
+        rotator.setCycleCount(INDEFINITE);
         return rotator;
     }
 
-    private void setWindupKeyMouseClicked() {
-        RotateTransition rotator = createRotator(windupKey);
-        rotator.play();
+    private void handleWindupKeyOnKeyUpPressed(KeyEvent event) {
+        if (event.getCode().equals(UP)) {
+            running = true;
+            if (rotator.getStatus().equals(RUNNING)) {
+                rotator.pause();
+            }
+            double rate = rotator.getRate();
+            rate = Math.min(rate + rate * RATE_MODIFIER, MAX_RATE);
+            rotator.setRate(rate);
+            rotator.play();
+        }
+    }
+
+    private void handleWindupKeyOnKeyUpReleased(KeyEvent event) {
+        if (event.getCode().equals(UP) && running) {
+            running = false;
+            executor.scheduleAtFixedRate(() -> {
+                if (rotator.getRate() >= RATE_MODIFIER) {
+                    rotator.pause();
+                    double rate = rotator.getRate() - rotator.getRate() * RATE_MODIFIER;
+                    rotator.setRate(rate);
+                    rotator.play();
+                } else {
+                    Thread.currentThread().interrupt();
+                }
+            }, 0, 1, SECONDS);
+        }
+    }
+
+    private void setupWindupKeyOnScroll() {
+        rotator = createRotator(windupKey);
     }
 
 }
