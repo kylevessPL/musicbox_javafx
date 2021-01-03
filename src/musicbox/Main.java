7f9febbed57dfd589dcd0e14fc9a3a0ed1d61c40
geekon <kacperpiasta@gmail.com>
2021-01-03 20:00:15 +0100

    add musicbox audio lullabies & adjust audio rate on keyup events

--- src/musicbox/Main.java
+++ src/musicbox/Main.java
@@ -1,7 +1,9 @@
 package musicbox;
 
+import javafx.animation.Animation;
 import javafx.animation.RotateTransition;
 import javafx.application.Application;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
@@ -22,12 +24,11 @@ import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.io.File;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static javafx.animation.Animation.INDEFINITE;
 import static javafx.animation.Animation.Status.RUNNING;
 import static javafx.animation.Interpolator.LINEAR;
 import static javafx.scene.input.KeyCode.UP;
@@ -39,24 +40,30 @@ public class Main extends Application {
     private static final String AUDIO01_VALUE = "AUDIO 01";
     private static final String AUDIO02_VALUE = "AUDIO 02";
 
-    private static final String AUDIO01_FILENAME = "D:\\java_test\\src\\musicbox\\boi.wav";
-    private static final String AUDIO02_FILENAME = "";
+    private static final String AUDIO01_FILENAME = "audio01.mp3";
+    private static final String AUDIO02_FILENAME = "audio02.mp3";
 
-    private static final double MAX_RATE = 20000;
-    private static final double RATE_MODIFIER = 0.25;
+    private static final double MIN_WINDUP_RATE = 3000;
+    private static final double MAX_WINDUP_RATE = 25000;
+    private static final double MIN_AUDIO_RATE = 1;
+    private static final double MAX_AUDIO_RATE = 1.15;
+    private static final double WINDUP_RATE_MODIFIER = 0.5;
+    private static final double AUDIO_RATE_MODIFIER = 0.02;
+
+    private final SimpleObjectProperty<MediaView> mediaView = new SimpleObjectProperty<>(this, "mediaView");
 
     private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+    private ScheduledFuture<?> task;
 
     private boolean running = false;
+    private double currentAudioRate = MIN_AUDIO_RATE;
 
+    private MediaPlayer player;
     private RotateTransition rotator;
-
     private ImageView windupKey;
     private TextField display;
     private ToggleGroup toggleGroup;
     private ToggleButton firstButton;
-
-    private final MediaView viewer = new MediaView();
 
     @Override
     public void start(Stage primaryStage) {
@@ -67,6 +74,7 @@ public class Main extends Application {
         primaryStage.setTitle("MusicBox");
         primaryStage.show();
         setupWindupKeyOnScroll();
+        setupAudio(AUDIO01_FILENAME);
         scene.addEventFilter(KEY_PRESSED, this::handleWindupKeyOnKeyUpPressed);
         scene.addEventFilter(KEY_RELEASED, this::handleWindupKeyOnKeyUpReleased);
         toggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
@@ -74,20 +82,13 @@ public class Main extends Application {
                 toggleGroup.selectToggle(oldValue);
             }
             display.setText(toggleGroup.getSelectedToggle() == firstButton ? AUDIO01_VALUE : AUDIO02_VALUE);
+            player.stop();
+            setupAudio(toggleGroup.getSelectedToggle() == firstButton ? AUDIO01_FILENAME : AUDIO02_FILENAME);
         });
-        playSound(AUDIO01_FILENAME);
     }
 
     public static void main(String[] args) {
         launch();
-    }
-
-    private void playSound(String filename) {
-        Media media = new Media(new File(filename).toURI().toString());
-        MediaPlayer player = new MediaPlayer(media);
-        viewer.setMediaPlayer(player);
-        player.play();
-        System.out.println(player.getStatus().toString());
     }
 
     private Scene createScene() {
@@ -155,8 +156,8 @@ public class Main extends Application {
         rotator.setFromAngle(0);
         rotator.setToAngle(360);
         rotator.setInterpolator(LINEAR);
-        rotator.setCycleCount(INDEFINITE);
-        rotator.setRate(RATE_MODIFIER);
+        rotator.setCycleCount(Animation.INDEFINITE);
+        rotator.setRate(WINDUP_RATE_MODIFIER);
         return rotator;
     }
 
@@ -166,24 +167,39 @@ public class Main extends Application {
             if (rotator.getStatus().equals(RUNNING)) {
                 rotator.pause();
             }
-            double rate = rotator.getRate();
-            rate = Math.min(rate + rate * RATE_MODIFIER, MAX_RATE);
-            rotator.setRate(rate);
+            double windupRate = rotator.getRate();
+            windupRate = Math.min(windupRate + windupRate * WINDUP_RATE_MODIFIER, MAX_WINDUP_RATE);
+            rotator.setRate(windupRate);
             rotator.play();
+            currentAudioRate = Math.min(currentAudioRate + currentAudioRate * AUDIO_RATE_MODIFIER, MAX_AUDIO_RATE);
+            player.setRate(currentAudioRate);
+            player.play();
         }
     }
 
     private void handleWindupKeyOnKeyUpReleased(KeyEvent event) {
         if (event.getCode().equals(UP) && running) {
             running = false;
-            executor.scheduleAtFixedRate(() -> {
-                if (rotator.getRate() >= RATE_MODIFIER) {
+            task = executor.scheduleAtFixedRate(() -> {
+                if (rotator.getRate() >= MIN_WINDUP_RATE) {
+                    if (running) {
+                        task.cancel(false);
+                        return;
+                    }
                     rotator.pause();
-                    double rate = rotator.getRate() - rotator.getRate() * RATE_MODIFIER;
-                    rotator.setRate(rate);
+                    double windupRate = rotator.getRate() - rotator.getRate() * WINDUP_RATE_MODIFIER / 2;
+                    rotator.setRate(windupRate);
                     rotator.play();
+                    double tempAudioRate = currentAudioRate - currentAudioRate * AUDIO_RATE_MODIFIER;
+                    currentAudioRate = Math.max(tempAudioRate, MIN_AUDIO_RATE);
+                    player.setRate(currentAudioRate);
+                    player.play();
+                    System.out.println(currentAudioRate);
                 } else {
-                    Thread.currentThread().interrupt();
+                    currentAudioRate = MIN_AUDIO_RATE;
+                    player.setRate(currentAudioRate);
+                    player.play();
+                    task.cancel(false);
                 }
             }, 0, 1, SECONDS);
         }
@@ -191,6 +207,17 @@ public class Main extends Application {
 
     private void setupWindupKeyOnScroll() {
         rotator = createRotator(windupKey);
+    }
+
+    private void setupAudio(String filename) {
+        Media media = new Media(getClass().getResource(filename).toExternalForm());
+        player = new MediaPlayer(media);
+        player.setCycleCount(MediaPlayer.INDEFINITE);
+        player.setRate(currentAudioRate);
+        mediaView.set(new MediaView(player));
+        if (rotator.getStatus() == RUNNING) {
+            player.play();
+        }
     }
 
 }
